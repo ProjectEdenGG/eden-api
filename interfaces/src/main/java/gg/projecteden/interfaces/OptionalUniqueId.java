4c5089a715c7f1c966552c10bf6be0ad0237ed14
Lexi <lexi@qixils.dev>
2022-06-14 23:10:00 -0400

    move interfaces to own module

--- interfaces/src/main/java/gg/projecteden/interfaces/OptionalUniqueId.java
+++ interfaces/src/main/java/gg/projecteden/interfaces/OptionalUniqueId.java
@@ -6,11 +6,7 @@ import java.util.UUID;
 
 /**
  * Represents an object that may have a {@link UUID}.
- * @deprecated Use Adventure's <a href="https://jd.adventure.kyori.net/api/4.9.3/net/kyori/adventure/pointer/Pointered.html">Pointered class</a>
- * with <a href="https://jd.adventure.kyori.net/api/4.9.3/net/kyori/adventure/identity/Identity.html#UUID">Identity.UUID</a>
  */
-@SuppressWarnings("DeprecatedIsStillUsed")
-@Deprecated(since = "2.0.0")
 public interface OptionalUniqueId {
 	/**
 	 * Returns a unique and persistent id for this object which may be null
