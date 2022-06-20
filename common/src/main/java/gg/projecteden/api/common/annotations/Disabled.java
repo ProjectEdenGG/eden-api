2f4d71ea798c921fa064a455ce41c4e806f058d4
GriffinCodes <griffin@projecteden.gg>
2022-06-20 16:59:25 -0500

    Allow Disabled annotation anywhere

--- common/src/main/java/gg/projecteden/api/common/annotations/Disabled.java
+++ common/src/main/java/gg/projecteden/api/common/annotations/Disabled.java
@@ -1,15 +1,12 @@
 package gg.projecteden.api.common.annotations;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 /**
  * Indicates that a class or method is disabled and should not be used.
  * Useful for objects that are loaded via reflection.
  */
-@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Disabled {
 }
