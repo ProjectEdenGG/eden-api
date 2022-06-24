fb4e70ebeeebcf998c9644f801529db02df90628
GriffinCodes <griffin@projecteden.gg>
2022-06-24 16:26:11 -0500

    Undo stricter generic typing

--- common/src/main/java/gg/projecteden/api/common/utils/EnumUtils.java
+++ common/src/main/java/gg/projecteden/api/common/utils/EnumUtils.java
@@ -14,28 +14,28 @@ public class EnumUtils {
 		throw new IllegalArgumentException();
 	}
 
-	private static <T extends IterableEnum> boolean hasNext(Class<? extends T> clazz, int ordinal) {
+	private static <T> boolean hasNext(Class<? extends T> clazz, int ordinal) {
 		T[] values = clazz.getEnumConstants();
 		return ordinal + 1 < values.length;
 	}
 
-	public static <T extends IterableEnum> T next(Class<? extends T> clazz, int ordinal) {
+	public static <T> T next(Class<? extends T> clazz, int ordinal) {
 		T[] values = clazz.getEnumConstants();
 		return values[Math.min(values.length - 1, ordinal + 1 % values.length)];
 	}
 
-	public static <T extends IterableEnum> T previous(Class<? extends T> clazz, int ordinal) {
+	public static <T> T previous(Class<? extends T> clazz, int ordinal) {
 		T[] values = clazz.getEnumConstants();
 		return values[Math.max(0, ordinal - 1 % values.length)];
 	}
 
-	public static <T extends IterableEnum> T nextWithLoop(Class<? extends T> clazz, int ordinal) {
+	public static <T> T nextWithLoop(Class<? extends T> clazz, int ordinal) {
 		T[] values = clazz.getEnumConstants();
 		int next = ordinal + 1 % values.length;
 		return next >= values.length ? values[0] : values[next];
 	}
 
-	public static <T extends IterableEnum> T previousWithLoop(Class<? extends T> clazz, int ordinal) {
+	public static <T> T previousWithLoop(Class<? extends T> clazz, int ordinal) {
 		T[] values = clazz.getEnumConstants();
 		int previous = ordinal - 1 % values.length;
 		return previous < 0 ? values[values.length - 1] : values[previous];
